810d099352c3fbd014aa875cb7ed23a2f9b86f45
geusan <dnay2k@gmail.com>
2017-08-03 09:45:38 +0000

    remove src

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Header from './Header';
-import Footer from './Footer';
-import Image from './Image';
-
-
-export { Header, Footer, Image };
--- src/containers/index.js
+++
@@ -1,9 +0,0 @@
-import App from './App';
-import Home from './Home';
-import Geusan from './Geusan';
-import Gamrom from './Gamrom';
-import Chayeoi from './Chayeoi';
-import Jeong from './Jeong';
-import Jiyun from './Jiyun';
-
-export { App, Home, Geusan, Gamrom, Chayeoi, Jeong, Jiyun };
