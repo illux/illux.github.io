fa8a61a8f30bf844043d2831bf162dce68197cfe
geusan <dnay2k@gmail.com>
2017-09-03 17:19:41 +0000

    geusan

---
+++ server/main.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import path from 'path';
+import WebpackDevServer from 'webpack-dev-server';
+import webpack from 'webpack';
+
+const app = express(); // express app 인스턴스
+
+const port = 8080;
+const devPort = 8082;
+
+app.use('/', express.static(path.join(__dirname, './../')));
+
+// static file 위치 / 으로 고정
+app.use(express.static('/'));
+
+// 기본 라우팅 위치 public/index.html
+app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './../index.html'));
+});
+
+app.listen(port, () => {
+    console.log('Express is listening on port', port);
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!' + err);
+});
+
+
+if(process.env.NODE_ENV == 'development') {
+    console.log('Server is running on development mode');
+    const config = require('../webpack.dev.config');
+    const compiler = webpack(config);
+    const devServer = new WebpackDevServer(compiler, config.devServer);
+    devServer.listen(
+        devPort, () => {
+            console.log('webpack-dev-server is listening on port', devPort);
+        }
+    );
+}
